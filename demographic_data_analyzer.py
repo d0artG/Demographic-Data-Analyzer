c4ba17f95fc3faace326e25bb2c039697c7a7cfc
d0artG <149911313+d0artG@users.noreply.github.com>
2024-08-04 11:32:06 +0100

    Starting point

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
+df = pd.read_csv("adult.data.csv")
+df.head()
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = "Wowzas"
+    df = pd.read_csv("adult.data.csv")
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
     race_count = None
